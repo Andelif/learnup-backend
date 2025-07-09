f8052d76a43c28dfa36cb0e3f10e0bf41682400e
Andelif Hossain <andelif.cse.20220104052@aust.edu>
2025-07-10 02:56:41 +0600

    Trying

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = '127.0.0.1'; // Change from "localhost" to "mysql" (Docker service name)
+$host = 'switchback.proxy.rlwy.net'; // Change from "localhost" to "mysql" (Docker service name)
 $username = "root"; // Default MySQL username
-$password = ''; // Match the MySQL password from Docker Compose
-$database = "learnup_db"; // Database name
+$password = 'XiSbWkhhWLDOHQCAadENepHFRXXYXZQQ'; // Match the MySQL password from Docker Compose
+$database = "railway"; // Database name
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $database);

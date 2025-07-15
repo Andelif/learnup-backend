d926230af0c589e61eb15c809585e556dbca692a
Andelif Hossain <andelif.cse.20220104052@aust.edu>
2025-07-15 16:38:25 +0600

    Fixing

--- config/db.php
+++ config/db.php
@@ -3,9 +3,10 @@ $host = 'mysql.railway.internal'; // Change from "localhost" to "mysql" (Docker 
 $username = "root"; // Default MySQL username
 $password = 'xuUhTMsdFrvdHpfjvrzSSLbrhUyXhRHb'; // Match the MySQL password from Docker Compose
 $database = "railway"; // Database name
+$port = 3306;
 
 // Create a connection
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database, $port);
 
 // Check connection
 if ($conn->connect_error) {
